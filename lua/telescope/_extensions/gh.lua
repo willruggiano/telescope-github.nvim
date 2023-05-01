9aef24aac4e29f59ceb4f1666aab23047ba35504
Will Ruggiano <wmruggiano@gmail.com>
2023-04-30 23:40:06 -0600

    feat: rename

--- lua/telescope/_extensions/gh.lua
+++ lua/telescope/_extensions/gh.lua
@@ -4,8 +4,8 @@ return require("telescope").register_extension {
   exports = {
     gist = gh_b.gh_gist,
     issues = gh_b.gh_issues,
-    pull_request = gh_b.gh_pull_request,
-    pull_request_files = gh_b.gh_pull_request_files,
+    pr = gh_b.gh_pull_request,
+    pr_files = gh_b.gh_pull_request_files,
     run = gh_b.gh_run,
     secret = gh_b.gh_secret,
   },
